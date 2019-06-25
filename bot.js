b44f334c1105e3884103e592a0c83da4ac2c45c9
IXFares99 <52209189+IXFares99@users.noreply.github.com>
2019-06-25 18:29:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("589421447974486047")
 setInterval(function() {
 channel.send(`5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra 5ra`);
-}, 10000000)
+}, 30)
 })
 
 client.login(process.env.BOT_TOKEN);
